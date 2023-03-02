69151ae52cfeba75616c54534b88046bf2ca9bb2
yuvarajm0418 <yuvarajm0418@gmail.com>
2023-03-02 14:12:30 +0530

    problems

--- prime.java
+++ prime.java
@@ -2,7 +2,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
-class hello{
+class prime{
   public static void inout(){
       try{
         System.setIn(new FileInputStream("input.txt"));
@@ -11,10 +11,18 @@ class hello{
         System.err.println("error");
       }
   }
-   public static void main(String args[]){
+public static void main(String args[]){
     inout();
     Scanner sc = new Scanner(System.in);
     int x =sc.nextInt();
-    System.out.println("HElo b  "+x);
+    boolean k=true;
+    for(int i=2;i<x/2;i++){
+      if(x%2==0){
+        k=false;
+        break;
+      }
+    }
+ if(k==false)System.out.println("Not Prime");
+  else System.out.println("True");
    }
 }
--- rev.java
+++ rev.java
@@ -2,7 +2,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
-class hello{
+class rev{
   public static void inout(){
       try{
         System.setIn(new FileInputStream("input.txt"));
@@ -11,10 +11,17 @@ class hello{
         System.err.println("error");
       }
   }
-   public static void main(String args[]){
+public static void main(String args[]){
     inout();
     Scanner sc = new Scanner(System.in);
     int x =sc.nextInt();
-    System.out.println("HElo b  "+x);
+    // System.out.println("HElo b  "+x);
+    int rev=0;
+    while(x>0){
+      int k =x%10;
+      rev =rev*10+k;
+      x/=10;
+    }
+    System.out.println(rev);
    }
 }
