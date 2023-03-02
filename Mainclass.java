053c2c389c11d979ab520eaead110e920be5033f
yuvarajm0418 <yuvarajm0418@gmail.com>
2023-03-02 12:50:52 +0530

    first commit

---
+++ Mainclass.java
@@ -0,0 +1,6 @@
+public class Mainclass{
+   public static void main(String args[]){
+
+    System.out.println("HElo");
+   }
+}
---
+++ hello.java
@@ -0,0 +1,21 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+class hello{
+  public static void inout(){
+      try{
+        System.setIn(new FileInputStream("input.txt"));
+        System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+      }catch(Exception e){
+        System.err.println("error");
+      }
+  }
+   public static void main(String args[]){
+    inout();
+    Scanner sc = new Scanner(System.in);
+    int x =sc.nextInt();
+    System.out.println("HElo b  "+x);
+
+   }
+}
